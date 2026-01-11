724a2cf519eb076c168ad7cf077d92b1d7e64b99
Leandro Rossi Sampaio <leandro.r.sampaio@gmail.com>
2026-01-11 02:10:22 +0100

    fix

--- src/objects/EnemyShip.ts
+++ src/objects/EnemyShip.ts
@@ -3,8 +3,6 @@ import Phaser from 'phaser';
 export class EnemyShip extends Phaser.Physics.Arcade.Sprite {
     public life: number = 4000;
     public shipName: string = 'USS Constellation';
-    private path: Phaser.Math.Vector2[];
-    private currentTarget: number = 0;
 
     constructor(scene: Phaser.Scene, x: number, y: number, playableWidth: number, gameHeight: number) {
         super(scene, x, y, 'enemy_ship');
@@ -13,6 +11,7 @@ export class EnemyShip extends Phaser.Physics.Arcade.Sprite {
         shipGraphics.fillStyle(0xffa500); // Orange color
         shipGraphics.fillRect(0, 0, 32, 32);
         shipGraphics.generateTexture('enemy_ship', 32, 32);
+        shipGraphics.destroy();
 
         scene.add.existing(this);
         scene.physics.add.existing(this);
@@ -20,59 +19,33 @@ export class EnemyShip extends Phaser.Physics.Arcade.Sprite {
         this.setInteractive();
         this.setCollideWorldBounds(true);
 
-        this.path = this.generateZPath(playableWidth, gameHeight);
-        this.moveToNextPoint();
+        this.startPatrol(playableWidth, gameHeight);
     }
 
-    private generateZPath(playableWidth: number, gameHeight: number): Phaser.Math.Vector2[] {
-        const path = [];
-        
-        // Start at bottom right
-        path.push(new Phaser.Math.Vector2(playableWidth - 50, gameHeight - 50));
-        // Top right
-        path.push(new Phaser.Math.Vector2(playableWidth - 50, 50));
-        // Top left
-        path.push(new Phaser.Math.Vector2(50, 50));
-        // Bottom left
-        path.push(new Phaser.Math.Vector2(50, gameHeight - 50));
-        // Bottom right
-        path.push(new Phaser.Math.Vector2(playableWidth - 50, gameHeight - 50));
+    private startPatrol(playableWidth: number, gameHeight: number) {
+        const margin = 100;
+        const speed = 20000; // Duration in ms
 
-        // Z back
-        // Bottom left
-        path.push(new Phaser.Math.Vector2(50, gameHeight - 50));
-        // Top left
-        path.push(new Phaser.Math.Vector2(50, 50));
-        // Top right
-        path.push(new Phaser.Math.Vector2(playableWidth - 50, 50));
-        // Start at bottom right
-        path.push(new Phaser.Math.Vector2(playableWidth - 50, gameHeight - 50));
+        const path = new Phaser.Curves.Path(this.x, this.y);
+        path.lineTo(playableWidth - margin, margin);
+        path.lineTo(margin, margin);
+        path.lineTo(margin, gameHeight - margin);
+        path.lineTo(this.x, this.y);
 
-        return path;
-    }
+        const follower = { t: 0, vec: new Phaser.Math.Vector2() };
+        this.scene.tweens.add({
+            targets: follower,
+            t: 1,
+            ease: 'Linear',
+            duration: speed,
+            yoyo: true,
+            repeat: -1
+        });
 
-    private moveToNextPoint() {
-        if (this.currentTarget >= this.path.length) {
-            this.currentTarget = 0;
-        }
-
-        const target = this.path[this.currentTarget];
-        this.scene.physics.moveTo(this, target.x, target.y, 100);
-        this.currentTarget++;
-    }
-
-    update() {
-        if (this.body && this.body.velocity.length() > 0) {
-            const distance = Phaser.Math.Distance.Between(
-                this.x,
-                this.y,
-                this.path[this.currentTarget - 1].x,
-                this.path[this.currentTarget - 1].y
-            );
-
-            if (distance < 4) {
-                this.moveToNextPoint();
-            }
-        }
+        this.scene.events.on('update', () => {
+            path.getPoint(follower.t, follower.vec);
+            this.setPosition(follower.vec.x, follower.vec.y);
+        });
     }
 }
+
